b7a45f9dd80471c7aac98112e0a2414935a58dac
Vido <vido@usp.br>
2015-10-24 18:19:35 -0200

    view sabor

--- campanha_clight/app/urls.py
+++ campanha_clight/app/urls.py
@@ -6,6 +6,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name="app/index.html"),name='index'),
-    url(r'^cor/(?P<palavra>[a-z]+)/$', views.cor,name='cor'),
+    url(r'^cor/(?P<palavra>[a-z]+)/$', views.cor, name='cor'),
+    url(r'^sabor/(?P<palavra>[a-z]+)/$', views.sabor, name='sabor'),
     url(r'^compartilhe/(?P<palavra>[a-z]+)/$', views.compartilhe,name='compartilhe'),
 ]
--- campanha_clight/app/views.py
+++ campanha_clight/app/views.py
@@ -1,88 +1,106 @@
+from django.http import Http404
 from django.shortcuts import render
 
 # Create your views here.
 
 def compartilhe(request, palavra):
-    textos = __getType(palavra)
-
-    context = {
-    'cor': textos['cor'],
-    'palavra': textos['palavra'],
-    'class': textos['class'],
-    }
+    context = __getType(palavra)
     return render(request,
                   'app/compartilhe.html',
                   context=context)
 
 def cor(request, palavra):
-    textos = __getType(palavra)
-
-    context = {
-    'cor': textos['cor'],
-    'palavra': textos['palavra'],
-    'class': textos['class'],
-    }
+    context = __getType(palavra)
     return render(request,
                   'app/cor.html',
                   context=context)
 
+def sabor(request, palavra):
+    context = __getType(palavra)
+    return render(request,
+                  'app/sabor.html',
+                  context=context)
 
 def __getType(palavra):
 
-    if(palavra == "romantica"):
+    if palavra == "romantica":
         mpalavra = "ROMÂNTICA"
         cor = "ROSA"
-        mclass="romantica"
-    elif(palavra == "livre"):
+        mclass = "romantica"
+        image = "produto-1.png"
+
+    elif palavra == "livre":
         mpalavra = "LIVRE"
         cor = "ROSA"
-        mclass="livre"
-    elif(palavra == "poderosa"):
+        mclass = "livre"
+
+    elif palavra == "poderosa":
         mpalavra = "PODEROSA"
         cor = "ROSA"
-        mclass="poderosa"
-    elif(palavra == "descolada"):
+        mclass = "poderosa"
+        image = "produto-1.png"
+
+    elif palavra == "descolada":
         mpalavra = "DESCOLADA"
         cor = "LARANJA"
-        mclass="descolada"
-    elif(palavra == "forte"):
+        mclass = "descolada"
+        image = "produto-1.png"
+
+    elif palavra == "forte":
         mpalavra = "FORTE"
         cor = "LARANJA"
-        mclass="forte"
-    elif(palavra == "alegre"):
+        mclass = "forte"
+        image = "produto-1.png"
+
+    elif palavra == "alegre":
         mpalavra = "ALEGRE"
         cor = "LARANJA"
-        mclass="alegre"
-    elif(palavra == "feliz"):
+        mclass = "alegre"
+        image = "produto-1.png"
+
+    elif palavra == "feliz":
         mpalavra = "FELIZ"
         cor = "VERDE"
-        mclass="feliz"
-    elif(palavra == "inspiradora"):
+        mclass = "feliz"
+        image = "produto-1.png"
+
+    elif palavra == "inspiradora":
         mpalavra = "INSPIRADORA"
         cor = "VERDE"
-        mclass="inspiradora"
-    elif(palavra == "otimista"):
+        mclass = "inspiradora"
+        image = "produto-1.png"
+
+    elif palavra == "otimista":
         mpalavra = "OTIMISTA"
         cor = "VERDE"
-        mclass="otimista"
-    elif(palavra == "sincera"):
+        mclass = "otimista"
+        image = "produto-1.png"
+
+    elif palavra == "sincera":
         mpalavra = "SINCERA"
         cor = "AZUL"
-        mclass="sincera"
-    elif(palavra == "tranquila"):
+        mclass = "sincera"
+        image = "produto-1.png"
+
+    elif palavra == "tranquila":
         mpalavra = "TRANQUILA"
         cor = "AZUL"
-        mclass="tranquila"
-    else:
+        mclass = "tranquila"
+        image = "produto-1.png"
+
+    elif palavra == "tranquila":
         mpalavra = "CONFIANTE"
         cor = "AZUL"
-        mclass="confiante"
+        mclass = "confiante"
+        image = "produto-1.png"
+    else:
+        raise Http404("Page not Found")
 
-    textos = {
+    context = {
         'cor': cor,
         'palavra': mpalavra,
         'class': mclass,
+        'image': image
     }
 
-    return textos
-
+    return context
