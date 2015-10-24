90fb567bc3cc6bd610619c96cac481beddae00b1
ismaelrodrigues <rodrigues.ismael@gmail.com>
2015-10-24 15:32:25 -0200

    cores e paginas

--- campanha_clight/app/views.py
+++ campanha_clight/app/views.py
@@ -6,6 +6,50 @@ def cor(request, palavra):
         mpalavra = "ROMÂNTICA"
         cor = "ROSA"
         mclass="romantica"
+    elif(palavra == "livre"):
+        mpalavra = "LIVRE"
+        cor = "ROSA"
+        mclass="livre"
+    elif(palavra == "poderosa"):
+        mpalavra = "PODEROSA"
+        cor = "ROSA"
+        mclass="poderosa"
+    elif(palavra == "descolada"):
+        mpalavra = "DESCOLADA"
+        cor = "LARANJA"
+        mclass="descolada"
+    elif(palavra == "forte"):
+        mpalavra = "FORTE"
+        cor = "LARANJA"
+        mclass="forte"
+    elif(palavra == "alegre"):
+        mpalavra = "ALEGRE"
+        cor = "LARANJA"
+        mclass="alegre"
+    elif(palavra == "feliz"):
+        mpalavra = "FELIZ"
+        cor = "VERDE"
+        mclass="feliz"
+    elif(palavra == "inspiradora"):
+        mpalavra = "INSPIRADORA"
+        cor = "VERDE"
+        mclass="inspiradora"
+    elif(palavra == "otimista"):
+        mpalavra = "OTIMISTA"
+        cor = "VERDE"
+        mclass="otimista"
+    elif(palavra == "sincera"):
+        mpalavra = "SINCERA"
+        cor = "AZUL"
+        mclass="sincera"
+    elif(palavra == "tranquila"):
+        mpalavra = "TRANQUILA"
+        cor = "AZUL"
+        mclass="tranquila"
+    else:
+        mpalavra = "CONFIANTE"
+        cor = "AZUL"
+        mclass="confiante"
 
     context = {
         'cor': cor,
