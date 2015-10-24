7ee5b41d0bbabd0139af3214c2e2caed7bd31858
ismaelrodrigues <rodrigues.ismael@gmail.com>
2015-10-24 14:01:14 -0200

    página cor

--- campanha_clight/app/urls.py
+++ campanha_clight/app/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name="app/index.html")),
+    url(r'^$', TemplateView.as_view(template_name="app/index.html"),name='index'),
+    url(r'^cor/', TemplateView.as_view(template_name="app/cor.html"),name='cor'),
 ]
