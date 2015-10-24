5c56c35ab3c08be30ba4ab59c2d0805a684775af
ismaelrodrigues <rodrigues.ismael@gmail.com>
2015-10-24 14:32:40 -0200

    Adicionando palavra

--- campanha_clight/app/urls.py
+++ campanha_clight/app/urls.py
@@ -3,5 +3,5 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name="app/index.html"),name='index'),
-    url(r'^cor/', TemplateView.as_view(template_name="app/cor.html"),name='cor'),
+    url(r'^cor/(?P<palavra>[a-z]+)/$', TemplateView.as_view(template_name="app/cor.html"),name='cor'),
 ]
--- campanha_clight/app/views.py
+++ campanha_clight/app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+def cor(request):
+    palavra = request.palavra
+    if(palavra == "romantica"):
+        palavra = "ROMÂNTICA"
+        cor = "ROSA"
+
+    context = {
+        'cor': cor,
+        'palavra': palavra,
+    }
+    return render(request,
+                  'app/cor.html',
+                  context=context)
+
